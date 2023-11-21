3461b835c0d1cff23bf2a043da22d961db8cbfa6
hgchoi <chg9252@gmail.com>
2023-11-21 17:21:27 +0900

    feat(): fragment 사용법

--- src/main/java/org/zerock/ex03/controller/SampleController.java
+++ src/main/java/org/zerock/ex03/controller/SampleController.java
@@ -59,4 +59,10 @@ public class SampleController {
     public void ex3() {
         log.info("ex3");
     }
+
+    @GetMapping({"/exLayout1","exLayout2"})
+    public void exLayout1(){
+        log.info("exLayout........");
+    }
+
 }
